5d0b60c497df238e1172b74ba27e37ce2c774f46
James Baldwin <jamescbaldwin@gmail.com>
2021-02-11 17:04:26 -0500

    contact form completed and working

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import About from "./components/About";
 import Services from "./components/Services";
 import Experience from "./components/Experience";
 import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+
 
 
 function App() {
@@ -38,6 +40,7 @@ function App() {
     <Services />
     <Experience />
     <Portfolio />
+    <Contact />
     </>
   );
 }
--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -3,7 +3,6 @@ import burger from "../assets/burgerApp.png";
 import directory from "../assets/employeeDirectory.png";
 import garage from "../assets/garageSwap.png";
 import wander from "../assets/wanderPlus.png"
-// import passwordGen from "../assets/passwordGenerator.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
 import { PopupboxManager, PopupboxContainer } from "react-popupbox";
@@ -11,30 +10,32 @@ import "react-popupbox/dist/react-popupbox.css";
 
 const Portfolio = () => {
 
+    /// Burger App
     const openPopupboxBurger = () => {
         const content = (
-        <>
-            <img className="portfolio-image-popupbox" src={burger} alt="Burger Handlebars App" />
-            <p>Sed consectetur massa sed metus elementum, blandit suscipit ante blandit. In venenatis tortor lorem, vitae placerat sapien lacinia vitae.</p>
-            <b>GitHub:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/jamescbaldwin/eat-da-burger")}>https://github.com/jamescbaldwin/eat-da-burger</a>
-        </>
-        )
-        PopupboxManager.open({ content })
-    }
+            <>
+                <img className="portfolio-image-popupbox" src={burger} alt="Burger Handlebars App..." />
+                <p>Sed consectetur massa sed metus elementum, blandit suscipit ante blandit. In venenatis tortor lorem, vitae placerat sapien lacinia vitae.</p>
+                <b>GitHub:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/jamescbaldwin/eat-da-burger")}>https://github.com/jamescbaldwin/eat-da-burger</a>
+            </>
+            )
+            PopupboxManager.open({ content })
+        }
 
     const popupboxConfigBurger = {
         titleBar: {
             enable: true,
-            text: "Eat Da Burger MVC App"
+            text: "Eat Da Burger MVC App",
         },
         fadeIn: true,
         fadeInSpeed: 500
     }
-    ///////////
+    
+    /// Wander Plus App
     const openPopupboxWander = () => {
         const content = (
         <>
-            <img className="portfolio-image-popupbox" src={wander} alt="WANDER + PLUS" />
+            <img className="portfolio-image-popupbox" src={wander} alt="WANDER + PLUS..." />
             <p>Sed consectetur massa sed metus elementum, blandit suscipit ante blandit. In venenatis tortor lorem, vitae placerat sapien lacinia vitae.</p>
             <b>GitHub:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/jamescbaldwin/WANDER-PLUS")}>https://github.com/jamescbaldwin/WANDER-PLUS</a>
         </>
@@ -50,11 +51,12 @@ const Portfolio = () => {
         fadeIn: true,
         fadeInSpeed: 500
     }
-    ////////////
+   
+    /// Garage Sale App
     const openPopupboxGarage = () => {
         const content = (
         <>
-            <img className="portfolio-image-popupbox" src={garage} alt="Garage-Swap" />
+            <img className="portfolio-image-popupbox" src={garage} alt="Garage-Swap..." />
             <p>Sed consectetur massa sed metus elementum, blandit suscipit ante blandit. In venenatis tortor lorem, vitae placerat sapien lacinia vitae.</p>
             <b>GitHub:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/jamescbaldwin/DEMO100")}>https://github.com/jamescbaldwin/DEMO100</a>
         </>
@@ -70,11 +72,12 @@ const Portfolio = () => {
         fadeIn: true,
         fadeInSpeed: 500
     }
-    ////////////
+    
+    /// Directory App
     const openPopupboxDirectory = () => {
         const content = (
         <>
-            <img className="portfolio-image-popupbox" src={directory} alt="Employee Directory" />
+            <img className="portfolio-image-popupbox" src={directory} alt="Employee Directory..." />
             <p>Sed consectetur massa sed metus elementum, blandit suscipit ante blandit. In venenatis tortor lorem, vitae placerat sapien lacinia vitae.</p>
             <b>GitHub:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/jamescbaldwin/react-employee-directory")}>https://github.com/jamescbaldwin/react-employee-directory</a>
         </>
@@ -97,25 +100,25 @@ const Portfolio = () => {
             <div className="container">
                 <h1 className="text-uppercase text-center py-5">portfolio</h1>
                 <div className="image-box-wrapper row justify-content-center">
-                <div className="portfolio-image-box" onClick={openPopupboxBurger}>
-                    <img className="portfolio-image" src={burger} alt="portfolio-image..."/>
-                    <div className="overflow"></div>
-                    <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
-                    </div>
-                <div className="portfolio-image-box" onClick={openPopupboxGarage}>
-                    <img className="portfolio-image" src={garage} alt="portfolio-image..."/>
-                    <div className="overflow"></div>
-                    <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
-                    </div>
-                <div className="portfolio-image-box" onClick={openPopupboxWander}>
-                    <img className="portfolio-image" src={wander} alt="portfolio-image..."/>
-                    <div className="overflow"></div>
-                    <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
-                    </div>
-                <div className="portfolio-image-box" onClick={openPopupboxDirectory}>
-                    <img className="portfolio-image" src={directory} alt="portfolio-image..."/>
-                    <div className="overflow"></div>
-                    <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+                    <div className="portfolio-image-box" onClick={openPopupboxBurger}>
+                        <img className="portfolio-image" src={burger} alt="portfolio-image..."/>
+                        <div className="overflow"></div>
+                            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+                        </div>
+                    <div className="portfolio-image-box" onClick={openPopupboxGarage}>
+                        <img className="portfolio-image" src={garage} alt="portfolio-image..."/>
+                        <div className="overflow"></div>
+                            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+                        </div>
+                    <div className="portfolio-image-box" onClick={openPopupboxWander}>
+                        <img className="portfolio-image" src={wander} alt="portfolio-image..."/>
+                        <div className="overflow"></div>
+                            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+                        </div>
+                    <div className="portfolio-image-box" onClick={openPopupboxDirectory}>
+                        <img className="portfolio-image" src={directory} alt="portfolio-image..."/>
+                        <div className="overflow"></div>
+                            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
                     </div>
                 </div>
             </div>
