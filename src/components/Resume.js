7fc8c50ab57444b277d089e4e64833ad75f7ac38
James Baldwin <jamescbaldwin@gmail.com>
2021-07-26 14:35:01 -0400

    margin between footer and resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,8 +5,9 @@ const Resume = () => {
     return (
         <div id="resume" className="resume-container">
             <div>
-                <h4 className="resume-header">Here's a snapshot of my current résumé .... to access as a PDF, click <a href="https://docs.google.com/document/d/1Yvm2z8AvEavDxrvmSpLpkIz2ftg7xWQz7Vl79leAbA4/export?format=pdf
-" target="_blank" >here</a> </h4>
+                <h1 className="resume-title">RÉSUMÉ</h1>
+                <h6 className="resume-header">Here's a snapshot of my current résumé .... to access as a PDF, click <a href="https://docs.google.com/document/d/1Yvm2z8AvEavDxrvmSpLpkIz2ftg7xWQz7Vl79leAbA4/export?format=pdf
+" target="_blank" >here</a> </h6>
             </div>
             <div>
                 <img  src={resumeImg} alt="resume" className="resume-screenshot" />
